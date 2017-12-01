d99630274bc1bf5178f6c43dfc61bbc459232294
5397chop <32233354+5397chop@users.noreply.github.com>
2017-12-01 15:29:00 -0600

    Add files via upload

---
+++ FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,33 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Ivor D. Addo on 11/30/17.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+
+    //MARK: Meal Class Tests
+    
+    func testMealInitializationSucceeds() {
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+    
+    func testMealInitializationFails() {
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+    }
+}
